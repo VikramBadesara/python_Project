624a24309a869acc57ceb9e252997b821509f27d
VikramBadesara <0.8vikrambadesara@gmail.com>
2023-03-03 19:29:01 +0530

    Added some more methods to list

--- main.py
+++ main.py
@@ -30,4 +30,19 @@ names1 = [["Viky", "Pooja"], ["Suyash", "Vishal"]]
 names1 = list(np.concatenate(names1))
 print(f'flatten a list using numpy: {names1}')
 
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+count = fruits.count('apple')
+print(f'Count of apples: {count}')
+index = fruits.index('banana')
+print(f'Index of banana in list: {index}')
+index = fruits.index('banana', 4)
+print(f'Find next banana starting a position 4: {index}')
+fruits.reverse()
+print(f'Reverse the list: {fruits}')
+fruits.sort()
+print(f'Sort list: {fruits}')
+print(f'removes last value of list: {fruits.pop()}')
+print(f'removes any index value of list: {fruits.pop(2)}')
+
 # all about dictionaries
+
