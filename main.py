d7a3242b302f357fa5cd11d7e085aa01e96b2745
VikramBadesara <0.8vikrambadesara@gmail.com>
2023-03-04 11:11:58 +0530

    Added about sets and dictionaries

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 # all about strings
+print(f'all about strings')
 name = "Viky Bad"
 print(f'hello, {name}')
 print(f'name: {name}')
@@ -13,6 +14,8 @@ print(f'count/length: {len(name)}')
 print(f'replace: {name.replace("Viky", "Vikram")}')
 
 # all about lists
+print(f'\n\nall about lists')
+
 names = ['Viky', "Pooja", "Vishal"]
 restNames = ["Suyash", "Rohan"]
 print(f'list length: {len(names)}')
@@ -43,6 +46,30 @@ fruits.sort()
 print(f'Sort list: {fruits}')
 print(f'removes last value of list: {fruits.pop()}')
 print(f'removes any index value of list: {fruits.pop(2)}')
+matrix = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        (9, 10, 11, "12"),
+        ]
+d = [[row[i] for row in matrix] for i in range(4)]
+print(f'convert 2D-Array to 1D: {d}')
+d = [row[i] for row in matrix for i in range(4)]
+print(f'flatten a 2d array: {d}')
+
+
+# all about sets
+print(f'\n\nall about sets')
+
+basket = {'apple', 'mango', 'grapes', "apple", "Apple"}
+print(f'set basket: {basket}')
+print(f'apple in basket: {"apple" in basket}')
+print(f'apple not in basket: {"apple" not in basket}')
+
 
 # all about dictionaries
+print(f'\n\nall about dictionaries')
+
+marks = {'Viky': 88, "Pooja": 89, "Sahil": 78}
+print(f'marks of Viky: {marks["Viky"]}')
+
 
